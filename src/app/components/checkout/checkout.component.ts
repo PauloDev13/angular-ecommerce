893ad9ea0d8745fa5be5e655b1738fce493da463
PgmAdmin <prmorais1302@gmail.com>
2023-09-17 21:48:08 -0300

    Implementing checkout page validation

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,9 +1,10 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { Country } from '../../common/country';
 import { State } from '../../common/state';
 import { Luv2ShopFormService } from '../../services/luv2-shop-form.service';
+import { Luv2ShopValidators } from '../../validators/luv2-shop.validator';
 
 @Component({
   selector: 'app-checkout',
@@ -30,6 +31,65 @@ export class CheckoutComponent implements OnInit {
   private readonly luv2ShopService: Luv2ShopFormService =
     inject(Luv2ShopFormService);
 
+  // formGroup cliente
+  get firstName() {
+    return this.checkoutFormGroup.get('customer.firstName');
+  }
+
+  get lastName() {
+    return this.checkoutFormGroup.get('customer.lastName');
+  }
+
+  get email() {
+    return this.checkoutFormGroup.get('customer.email');
+  }
+
+  // formGroup endereço entrega
+  get country() {
+    return this.checkoutFormGroup.get('shippingAddress.country');
+  }
+
+  get street() {
+    return this.checkoutFormGroup.get('shippingAddress.street');
+  }
+
+  get city() {
+    return this.checkoutFormGroup.get('shippingAddress.city');
+  }
+
+  get state() {
+    return this.checkoutFormGroup.get('shippingAddress.state');
+  }
+
+  get zipCode() {
+    return this.checkoutFormGroup.get('shippingAddress.zipCode');
+  }
+
+  // formGroup cartão de crédito
+  get cardType() {
+    return this.checkoutFormGroup.get('creditCard.cardType');
+  }
+
+  get nameOnCard() {
+    return this.checkoutFormGroup.get('creditCard.nameOnCard');
+  }
+
+  get cardNumber() {
+    return this.checkoutFormGroup.get('creditCard.cardNumber');
+  }
+
+  get securityCode() {
+    return this.checkoutFormGroup.get('creditCard.securityCode');
+  }
+
+  get expirationMonth() {
+    return this.checkoutFormGroup.get('creditCard.expirationMonth');
+  }
+
+  get expirationYear() {
+    return this.checkoutFormGroup.get('creditCard.expirationYear');
+  }
+
   ngOnInit() {
     this.getCreditCardYears();
     this.getCreditCardMonths(this.startMonth);
@@ -37,41 +97,100 @@ export class CheckoutComponent implements OnInit {
 
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
-        firstName: [''],
-        lastName: [''],
-        email: [''],
+        firstName: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(2),
+            Luv2ShopValidators.notOnlyWhitespace,
+          ],
+        ],
+        lastName: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(2),
+            Luv2ShopValidators.notOnlyWhitespace,
+          ],
+        ],
+        email: [
+          '',
+          [
+            Validators.required,
+            Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
+          ],
+        ],
       }),
       shippingAddress: this.formBuilder.group({
-        country: [''],
-        street: [''],
-        city: [''],
-        state: [''],
-        zipCode: [''],
+        country: ['', [Validators.required]],
+        street: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(2),
+            Luv2ShopValidators.notOnlyWhitespace,
+          ],
+        ],
+        city: ['', [Validators.required]],
+        state: ['', [Validators.required]],
+        zipCode: [
+          '',
+          [Validators.required, Luv2ShopValidators.notOnlyWhitespace],
+        ],
       }),
       billingAddress: this.formBuilder.group({
-        country: [''],
-        street: [''],
-        city: [''],
-        state: [''],
-        zipCode: [''],
+        country: ['', [Validators.required]],
+        street: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(2),
+            Luv2ShopValidators.notOnlyWhitespace,
+          ],
+        ],
+        city: ['', [Validators.required]],
+        state: ['', [Validators.required]],
+        zipCode: ['', Validators.required],
       }),
       creditCard: this.formBuilder.group({
-        cardType: [''],
-        nameOnCard: [''],
-        cardNumber: [''],
-        securityCode: [''],
-        expirationMonth: [''],
-        expirationYear: [''],
+        cardType: ['', [Validators.required]],
+        nameOnCard: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(2),
+            Luv2ShopValidators.notOnlyWhitespace,
+          ],
+        ],
+        cardNumber: [
+          '',
+          [
+            Validators.required,
+            Validators.pattern('^[0-9]{16}'),
+            Luv2ShopValidators.notOnlyWhitespace,
+          ],
+        ],
+        securityCode: [
+          '',
+          [
+            Validators.required,
+            Validators.pattern('^[0-9]{3}'),
+            Luv2ShopValidators.notOnlyWhitespace,
+          ],
+        ],
+        expirationMonth: ['', [Validators.required]],
+        expirationYear: ['', [Validators.required]],
       }),
     });
   }
 
   onSubmit() {
-    console.log('Submit');
-    console.log(this.checkoutFormGroup.get('customer')?.value);
-    console.log(this.checkoutFormGroup.get('shippingAddress')?.value);
-    console.log(this.checkoutFormGroup.get('billingAddress')?.value);
-    console.log(this.checkoutFormGroup.get('creditCard')?.value);
+    if (this.checkoutFormGroup.invalid) {
+      this.checkoutFormGroup.markAllAsTouched();
+    }
+    console.log(this.checkoutFormGroup.get('customer.firstName')?.value);
+    console.log(this.checkoutFormGroup.get('customer.lastName')?.value);
+    console.log(this.checkoutFormGroup.get('customer.email')?.value);
   }
 
   copyShippingAddressToBillingAddress(event: Event) {
---
+++ src/app/validators/luv2-shop.validator.spec.ts
@@ -0,0 +1,7 @@
+import { Luv2ShopValidators } from './luv2-shop.validator';
+
+describe('Luv2Shop', () => {
+  it('should create an instance', () => {
+    expect(new Luv2ShopValidators()).toBeTruthy();
+  });
+});
