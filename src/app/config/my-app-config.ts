9a3e37fafe8dfd5c2039802acad3cd18acad69d0
PgmAdmin <prmorais1302@gmail.com>
2023-09-21 12:18:58 -0300

    Installing dependencies and configuring Okta params

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,8 @@
+export default {
+  oidc: {
+    clientId: '0oa7vht4nf1E7VCU3697',
+    issuer: 'https://trial-8805994.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:4200/login/callback',
+    scopes: ['openid', 'profile', 'email'],
+  },
+};
